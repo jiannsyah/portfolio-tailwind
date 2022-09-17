585ea2c19f368ab39651967ef4d9c94eaabb2c76
jiannsyah <brian8jian@gmail.com>
2022-09-18 01:26:45 +0700

    selesai

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,19 @@
-const hamburger = document.querySelector("#hamburger");
+//Navbar fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  //   console.log(window.pageYOffset);
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
 
+//Hamburger
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
 });
